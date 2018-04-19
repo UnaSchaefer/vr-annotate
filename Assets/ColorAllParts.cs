ee59ac2628e44e6a1c6037c237162fad5a62d652
Una Schaefer <una.ulrike.schaefer@hu-berlin.de>
2018-04-19 14:09:00 +0200

    New scene settings; RockVR VideoCaptureAsset integration

---
+++ Assets/ColorAllParts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorAllParts : MonoBehaviour {
+
+    private Component[] allRenderer;
+    public Material mat;
+	// Use this for initialization
+	void Start () {
+        allRenderer = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer rnd in allRenderer)
+        {
+            rnd.material = mat;   
+        }
+        //Material mat = new Material(Shader.Find("Standard"));
+        //Material mat = new Material(Shader.Find("test"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
